a7423efe3f31f939024f476bf2b0c8b1592c1527
Minifixio <emile@le-gallic.com>
2022-11-07 21:27:07 +0100

    Added API service for Angular

---
+++ WewDashboardWeb/src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  port = 3000
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  async get<T>(service: string, endpoint: string) {
+    const url = `http://localhost:${this.port}/${service}/${endpoint}`
+    return this.http.get<T>(url).toPromise()
+  }
+}
--- WewDashboardWeb/src/app/services/http.service.ts
+++ WewDashboardWeb/src/app/services/http.service.ts
@@ -18,13 +18,12 @@ export class HttpService {
     private http: HttpClient,
     private error: ErrorsService
   ) { }
-  
 
   async getOpenWeatherMap<T>(type: string, version: string, tag: string,  params: Param[]) {
     const paramsUrl = params.map((param) => {return `${param.name}=${param.value}` }).join('&')
     const url = `https://api.openweathermap.org/${type}/${version}/${tag}?${paramsUrl}&appid=${openWeatherMapApiKey}&units=metric&lang=en`
     try {
-      return await this.http.get<T>(url).toPromise()
+      return this.http.get<T>(url).toPromise()
     } catch(e) {
       this.error.promptError('error with OpenWeatherMap API : ' + e)
       throw e

c476950192da3a35858520fc43df2a2c5b5e284a
Minifixio <emile@le-gallic.com>
2022-11-09 20:48:29 +0100

    updated Angular to latest

--- WewDashboardWeb/src/app/app.module.ts
+++ WewDashboardWeb/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +9,6 @@ import { MeteoComponent } from './components/meteo/meteo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MeteoCardTodayComponent } from './components/meteo-card-today/meteo-card-today.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
@@ -29,9 +27,7 @@ import {MatIconModule} from '@angular/material/icon';
     MeteoCardTodayComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatGridListModule,
     HttpClientModule,
     MatCardModule,
